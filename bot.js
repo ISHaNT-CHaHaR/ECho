832eb2a21e97f152b8b2870169f23cbea150fa07
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-04-07 14:55:25 +0530

    created an echo bot
    .

--- bot.js
+++ bot.js
@@ -1,46 +1,44 @@
 const Telegraf = require('telegraf');
 
-const bot = new Telegraf('1126313437:AAEeJMWbpzvtL_wf9BT20Pcq9XY8qajcJnU');
-/// code to handle the update and messages.
+const bot = new Telegraf('1231488835:AAF88PEIq3WYnqc8hYwi7YHgbZg4qyUfkjo');
 
-bot.hears('maal laaya?', (ctx) => {
-   ctx.reply('Tu rokda laaya?');
-});
-bot.hears('ye kya bag bhrke laaya!', (ctx) => {
-   ctx.reply('ok');
-});
-
-bot.on('sticker', (ctx) => {
-   ctx.reply('sticker message h');
-});
-
-bot.mention('botfather', (ctx) => {
-   ctx.reply('mention method1');
-});
-
-bot.phone('+1 931 962-2527', (ctx) => {
-   ctx.reply('Phone');
-});
-bot.hashtag('hash', (ctx) => {
-   ctx.reply('HAshtag mentioned');
-});
+const helpMessage = `
+Say something to me.
+/start - start the bot
+/help - command reference
+`;
 
 bot.use((ctx, next) => {
-   ctx.state.apple = 5;
-
-   ctx.reply('You used this peace of art.');
+   if (ctx.updateSubTypes[0] == 'text') {
+      /// beacause updateSubType is an array
+      console.log(ctx.from.first_name + ' said: ' + ctx.message.text);
+   } else {
+      console.log(ctx.from.first_name + ' said: ' + ctx.updateSubTypes[0]);
+   }
    next();
 });
-
 bot.start((ctx) => {
-   bot.telegram.sendMessage(ctx.chat.id, 'Hello World!', {
-      parse_mode: 'Markdown',
-      disable_notification: true, ///Extra parameter object.
-   });
-   ctx.reply('Hello', {
-      parse_mode: 'Markdown',
-      disable_notification: true,
-   });
+   ctx.reply('Hi I am Echo bot');
+
+   ctx.reply(helpMessage);
+});
+
+bot.help((ctx) => {
+   ctx.reply(helpMessage);
+});
+
+bot.command('echo', (ctx) => {
+   let input = ctx.message.text;
+   let inputArray = input.split(' ');
+   let message = '';
+   if (inputArray.length === 1) {
+      // check if array contains echo.
+      message = 'You said echo';
+   } else {
+      inputArray.shift(); //removes 1st element in arrray
+      message = inputArray.join(' ');
+   }
+   ctx.reply(message);
 });
 
 bot.launch();
