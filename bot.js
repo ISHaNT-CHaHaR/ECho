de3896434b05bc73500618b41f75a55d9194dfe1
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-04-07 15:30:37 +0530

    completed echo bot.
    .

--- bot.js
+++ bot.js
@@ -9,12 +9,25 @@ Say something to me.
 `;
 
 bot.use((ctx, next) => {
+   // if (ctx.updateSubTypes[0] == 'text') {
+   //    /// beacause updateSubType is an array
+   //    console.log(ctx.from.first_name + ' said: ' + ctx.message.text);
+   // } else {
+   //    console.log(ctx.from.first_name + ' said: ' + ctx.updateSubTypes[0]);
+   // }
+
    if (ctx.updateSubTypes[0] == 'text') {
-      /// beacause updateSubType is an array
-      console.log(ctx.from.first_name + ' said: ' + ctx.message.text);
+      bot.telegram.sendMessage(
+         ctx.chat.id,
+         ctx.from.first_name + ' said: ' + ctx.message.text
+      );
    } else {
-      console.log(ctx.from.first_name + ' said: ' + ctx.updateSubTypes[0]);
+      bot.telegram.sendMessage(
+         ctx.chat.id,
+         ctx.from.first_name + ' said: ' + ctx.updateSubTypes[0]
+      );
    }
+
    next();
 });
 bot.start((ctx) => {
